047649e6e7768ed2486b5e70cb55e2da2a288135
Md. Arish <arish123.ahamad@gmail.com>
2023-06-11 03:41:26 +0530

    Time: 11 ms (37.77%), Space: 7 MB (65.50%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> arr;
-        map<char,char> hsh;
+        unordered_map<char,char> arr;
+        unordered_map<char,char> hsh;
         
         for(int i=0;i<s.size();i++){
-            cout<<s[i]<<" "<<t[i]<<" "<<(arr.find(s[i])==arr.end())<<"\n";
+            // cout<<s[i]<<" "<<t[i]<<" "<<(arr.find(s[i])==arr.end())<<"\n";
             if(arr.find(s[i])==arr.end() && hsh.find(t[i])==hsh.end()){
                 hsh[t[i]]=s[i];
                 arr[s[i]]=t[i];
