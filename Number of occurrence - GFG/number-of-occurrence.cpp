03bb328cc6d7ed5d9d0ea5e725156e8a6eada576
Md. Arish <arish123.ahamad@gmail.com>
2023-07-14 00:58:25 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,10 +10,59 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
+	
+	int lower(int arr[],int n, int x){
+	    int low=0;
+	    int high=n-1;
+	    int ans=-1;
+	    while(low<=high)
+	    {
+	        int mid=(low+high)/2;
+	        if(arr[mid]==x)
+	        {
+	            ans=mid;
+	            high=mid-1;
+	        }
+	        else if(arr[mid]<x)
+	        {
+	            low=mid+1;
+	        }
+	        else{
+	            high=mid-1;
+	        }
+	    }
+	    return ans;
+	}	
+		
+	int upper(int arr[],int n, int x){
+	    int low=0;
+	    int high=n-1;
+	    int ans=-1;
+	    while(low<=high)
+	    {
+	        int mid=(low+high)/2;
+	        if(arr[mid]==x)
+	        {
+	            ans=mid;
+	            low=mid+1;
+	        }
+	        else if(arr[mid]<x)
+	        {
+	            low=mid+1;
+	        }
+	        else{
+	            high=mid-1;
+	        }
+	    }
+	    return ans;
+	}	
+	
 	int count(int arr[], int n, int x) {
-	    int i=upper_bound(arr,arr+n,x)-arr;
-	    int j=lower_bound(arr,arr+n,x)-arr;
-	    return i-j;
+	    int i=upper(arr,n,x);  // log(n)
+	   // cout<<"asdf"<<i<<"\n";
+	    int j=lower(arr,n,x);  // log(n)
+	    if(i==-1 || j==-1) return 0;
+	    return i-j+1;
 	}
 };
 
